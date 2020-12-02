0f688c34cfc694c83b2165d5d398a96b5c424360
Vasily Blinkov <vasily_blinkov@live.com>
2020-12-02 08:26:00 +0300

    Preparation for deployment from local Git

--- deployment/auto-sync.ps1
+++ deployment/auto-sync.ps1
@@ -2,14 +2,15 @@
 
 $parameters=get-content "$PSScriptRoot\parameters.json" | convertfrom-json
 
+#TODO: Проверить, что приложение отсутствует; если отсутствует, выполнить следующую кмд.
 az webapp create `
  --subscription $parameters.subscription `
  --name $parameters.webapp `
  --plan $parameters.appservicePlan `
  --resource-group $parameters.resourceGroup `
- --deployment-source-url $parameters.githubRepo
-
-az webapp deployment source config `
+ --deployment-local-git
+#TODO: Иначе (т.е. если приложение существует), выполнить следующую кмд, предварительно исправив ее согласно `az webapp deployment source config-local-git -h`.
+az webapp deployment source config-local-git `
  --subscription $parameters.subscription `
  --resource-group $parameters.resourceGroup `
  --name $parameters.webapp `
